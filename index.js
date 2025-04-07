e6c7d43c043f1e28456c76709a247d4c1c5fa11d
ummarmtp <ummarmtp@gmail.com>
2025-04-07 11:49:42 +0400

    mqtt

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 3000;
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.post('/data', (req, res) => {
-  console.log('Received JSON:', req.body);
-  res.sendStatus(200);
+// app.post('/data', (req, res) => {
+//   console.log('Received JSON:', req.body);
+//   res.sendStatus(200);
+// });
+
+// app.listen(port, () => {
+//   console.log(`HTTP server listening on port ${port}`);
+// });
+
+
+
+const http = require("http");
+const websocket = require("websocket-stream");
+const aedes = require("aedes")();
+
+const server = http.createServer();
+
+websocket.createServer({ server }, aedes.handle);
+
+// Default Render port
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, () => {
+  console.log(`MQTT broker running on ws://localhost:${PORT}`);
 });
-
-app.listen(port, () => {
-  console.log(`HTTP server listening on port ${port}`);
-});
+s
